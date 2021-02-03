b5aad9b2e01ea04456a45262ec7636f27a6d45cb
Raphael Reis <rapphha@gmail.com>
2021-02-02 21:27:54 -0300

    specific endpoint

--- src/main/kotlin/microservice/MicroserviceApplication.kt
+++ src/main/kotlin/microservice/MicroserviceApplication.kt
@@ -7,6 +7,8 @@ import org.springframework.data.relational.core.mapping.Table
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
@@ -17,24 +19,23 @@ fun main(args: Array<String>) {
 }
 
 @RestController
-class MessageResource(val sevice: MessageService) {
+class MessageResource(val service: MessageService) {
     @GetMapping
-    fun index(): List<Message> = listOf(
-            Message("1", "hello"),
-            Message("2", "holla")
-    )
+    fun index(): List<Message> = service.findMessage()
 
+    @PostMapping
+    fun post(@RequestBody message: Message) {
+        service.post(message)
+    }
 }
 
 @Service
-class MessageService {
+class MessageService(val db: MessageRepository) {
 
-    fun findMessage(): List<Message> {
-        TODO()
-    }
+    fun findMessage(): List<Message> = db.findMessages()
 
     fun post(message: Message) {
-        TODO()
+        db.save(message)
     }
 }
 
